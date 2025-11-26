57b2fd990e7b566890989859a168697d884ccbd0
Kunal628-hue <creationofkunal@gmail.com>
2025-11-26 14:59:41 +0530

    added an extra feature of themes

--- JS/constants.js
+++ JS/constants.js
@@ -1,5 +1,6 @@
 export const NOTES_STORAGE_PREFIX = "notesWorkspace.notes";
 export const ACTIVE_USER_KEY = "notesWorkspace.activeUser";
 export const ACCOUNT_KEY = "notesWorkspace.accounts.v1";
+export const THEME_KEY = "notesWorkspace.theme";
 
 
